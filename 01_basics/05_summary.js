90678eb9731e3305ed6246cf6478bbb080272797
Saurabh Sen <102850698+smile2504@users.noreply.github.com>
2023-10-22 03:56:14 +0000

    summary

--- 01_basics/05_summary.js
+++ 01_basics/05_summary.js
@@ -37,4 +37,39 @@ const myFunction = function(){
 console.log(typeof myFunction);
 
 
+/***********************************************************************/
 
+//----> Memory Allocation
+
+//-> Stack memory allocation for Primitive data type: 
+//In this we have given the copy of data and modification is also done on that copied data.
+// Original value in the memory will not be modified
+// It is call by value
+
+let myName = "Saurabh"
+let myPetName = myName
+console.log(myName);
+console.log(myPetName); 
+
+myPetName = "Laltu"
+console.log(myName);
+console.log(myPetName);  // In this we can see that the value changes in "myPetName" didn't change the original value of "myName"
+
+
+//-> Heap memory allocation for Non-Primitive data
+// In this we give reference of original data and modification is done on original data
+//Original value in memory will be modified
+//It is call by reference
+
+let userOne = {
+    email: "ss@gmail.com",
+    uid: "21BCS8101"
+}
+
+let userTwo = userOne
+console.log(userOne.email); // To get the value from object
+console.log(userTwo.email);
+
+userTwo.email = "ss-7@gmail.com";
+console.log(userOne.email);
+console.log(userTwo.email);  // Here we will see that by changing the email in "userTwo", there will be also modification of email in "userOne"
